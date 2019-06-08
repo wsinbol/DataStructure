1e2d800fcc1f3d133bccad7c33e05c59e0f95478
wsinbol <wsinbol@gmail.com>
2019-06-08 22:14:02 +0800

    add desc to double_linked_list

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -8,6 +8,15 @@ class Node(object):
 		self._next = _next
 
 class DoubleLinkedList(object):
+	'''
+	方法：
+	insert_new_value_to_head 在头部插入新结点
+	insert_new_value_after_target_node 在给定结点后插入新结点
+	insert_new_value_before_target_node 在给定结点前插入新结点
+	delete_target_node 删除给定结点
+	print_double_linked_list 打印链表信息
+	get_node_by_value 返回给定结点
+	'''
 
 	def __init__(self, node=None):
 		self._head = node
@@ -37,6 +46,10 @@ class DoubleLinkedList(object):
 		new_node._next = target_node
 		target_node._prev = new_node
 
+	def delete_target_node(self, target_node):
+		target_node._next._prev = target_node._prev
+		target_node._prev._next = target_node._next
+
 
 	def print_double_linked_list(self):
 		current = self._head
@@ -46,7 +59,7 @@ class DoubleLinkedList(object):
 			else:
 				print(f"⇄{current.data}", end="")
 			current = current._next
-			
+
 
 	def get_node_by_value(self, value):
 		current = self._head
@@ -86,6 +99,10 @@ if __name__ == '__main__':
 	data2 = dl.get_node_by_value(2)
 	dl.insert_new_value_after_target_node(data2, 3)
 	dl.insert_new_value_after_target_node(data2, 4)
+
+	# 删除数据4这个结点
+	data4 = dl.get_node_by_value(4)
+	dl.delete_target_node(data4)
 	
 
 	'''
