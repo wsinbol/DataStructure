2e2031fa48c6bab0945c29da879c70202c02b2ec
wsinbol <wsinbol@gmail.com>
2019-07-18 16:54:57 +0800

    CircularQueue

---
+++ Queue/CircularQueue.py
@@ -0,0 +1,49 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+class CircularQueue(object):
+
+	def __init__(self, capacity):
+		self._items = [] # 基于列表实现
+		# 根据循环队列的特性，大小为capacity的队列最多只能存储capacity-1大小的数据
+		# 队满时的tail位置并不存储数据，所以循环队列会浪费一个存储空间
+		# 针对这种情况，避免造成和数组等类型的不一致，所以再封装循环队列的时候可以将capacity+1以保证确实能存储capacity个数据
+		# 这样也不会给调用者造成迷惑
+		self._capacity = capacity
+		self._head = 0
+		self._tail = 0
+
+	def enqueue(self, value):
+		if (self._tail + 1) % self._capacity == self._head:
+			print('The queue is full! The data %s ignored!' % value)
+			return False
+		self._items.append(value)
+		self._tail = (self._tail + 1) % self._capacity
+	
+	def dequeue(self):
+		if self._head == self._tail:
+			print('The queue is empty')
+			return False
+		value = self._items[self._head]
+		self._head = (self._head + 1) % self._capacity
+		return value
+
+	def printQueue(self):
+		current = self._head
+		while current != self._tail:
+			print(self._items[current], end=',')
+			current = (current + 1) % self._capacity
+		print()
+
+if __name__ == '__main__':
+	q = CircularQueue(5)
+	q.enqueue(2)
+	q.enqueue(0)
+	q.enqueue(1)
+	q.enqueue(3)
+	q.printQueue()
+	q.enqueue(2)
+	q.enqueue(4)
+	for _ in range(6):
+		print(q.dequeue())
+	q.printQueue()
