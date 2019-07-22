bd43f6dc561d09ca44496f3f091d8bfc9913b41a
asus <asus@symbol.com>
2019-07-22 18:27:31 +0800

    selection sort

--- Sort/InsertionSort.py
+++ Sort/InsertionSort.py
@@ -7,15 +7,15 @@
 	将未排元素置于已排区间的合适位置
 '''
 
-def InsertSort(arr):
+def InsertionSort(arr):
 	length = len(arr)
 	for i in range(1,length):# 未排序好的队列
 		value = arr[i]
 		for j in range(i-1,-1,-1): # 已排序好的队列
 			if arr[j] > value:
 				arr[j+1] = arr[j]
-				arr[j] = value 
-			# 不后移数据时，会将数据覆盖丢失
+				arr[j] = value  # 此时不更新，就不知道应该啥时候更新了
+			# 不转移数据时，会将数据覆盖丢失
 			# arr[j] = value
 		# 最终的j都会是0,所以不可以在这里更新arr[j]的值
 		# arr[j] = value
@@ -23,7 +23,7 @@ def InsertSort(arr):
 
 
 '''
-def InsertSort(arr):
+def InsertionSort(arr):
 	length = len(arr)
 	for i in range(1,length):
 		value = arr[i]
@@ -36,6 +36,6 @@ def InsertSort(arr):
 
 if __name__ == '__main__':
 	arr = [3,5,1,4]
-	InsertSort(arr)
+	InsertionSort(arr)
 	# print(arr)
 
