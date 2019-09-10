7f2e2629c5c32bcc80cae732a5691135d5eeb1dd
asus <asus@symbol.com>
2019-09-10 12:59:20 +0800

    基于开放寻址法-线性探测的散列表

--- HashTable/SimplyHashTable.py
+++ HashTable/SimplyHashTable.py
@@ -1,8 +1,7 @@
 # -*- coding:utf-8 -*-
-# 
-
-
-
+'''
+基于开放寻址法-线性探测的散列表
+'''
 class HashTable(object):
 	def __init__(self, _size = 0):
 		self._table = [None for i in range(_size)]
@@ -12,11 +11,12 @@ class HashTable(object):
 		return key % self._size
 
 	def insert(self, val):
-		if self._table[self.hash_function(val)] == None:
+		# 千万别用 == 
+		if self._table[self.hash_function(val)] is None or self._table[self.hash_function(val)] is False:
 			self._table[self.hash_function(val)] = val
 		else:
 			key = self.linear_probing(self.hash_function(val))
-			if key:
+			if key != None:
 				print(val,'-触发寻址,原本下标为:',self.hash_function(val),'实际下标为:',key)
 				self._table[key] = val
 			else:
@@ -28,7 +28,9 @@ class HashTable(object):
 			return key
 
 		start_key = key + 1
-		while self._table[start_key] != None and key != start_key:
+		if start_key == self._size:
+			start_key = 0
+		while self._table[start_key] is not None and key != start_key:
 			if self._table[start_key] == val:
 				return start_key
 			else:
@@ -37,6 +39,29 @@ class HashTable(object):
 					start_key = 0
 		return None
 
+	def delete(self, val):
+		key = self.hash_function(val)
+		if self._table[key] == val:
+			self._table[key] = False
+			return
+
+		start_key = key + 1
+		is_loop = 0
+
+		if start_key == self._size:
+			start_key = 0
+
+		while not is_loop:
+			if self._table[start_key] == val:
+				self._table[start_key] = False
+				break
+			else:
+				start_key += 1
+				if start_key == self._size:
+					start_key = 0
+				if start_key+1 == key:
+					is_loop = 1
+		return None
 
 	'''
 	线性探测寻址
@@ -46,7 +71,8 @@ class HashTable(object):
 		while start_key != key:
 			if start_key == self._size:
 				start_key = 0
-			if self._table[start_key] == None:
+			#  0 == Fasle return True; so replace with 'is False'
+			if self._table[start_key] is None or self._table[start_key] is False:
 				return start_key
 			else:
 				start_key += 1
@@ -58,16 +84,20 @@ class HashTable(object):
 
 if __name__ == '__main__':
 	h = HashTable(6)
-	# h.print_table()
 	h.insert(12)
 	h.insert(0)
 	h.insert(5)
 	h.insert(11)
-	h.insert(9)
-	h.insert(10)
-	h.insert(2)
+	h.insert(15)
+	h.print_table()
+	print('delete...')
+	h.delete(5)
+	h.delete(15)
+	h.print_table()
+	print('insert...')
+	h.insert(17)
+	h.insert(23)
+	print()
 	h.print_table()
 
-	print('Search....')
-	print(h.search(0))
 
