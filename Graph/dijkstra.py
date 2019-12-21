9b1daf6301111d969950ed713a4fa0186bffad36
wsinbol <wsinbol@gmail.com>
2019-12-21 21:09:45 +0800

    update

--- Graph/dijkstra.py
+++ Graph/dijkstra.py
@@ -1,8 +1,11 @@
+'''
+@Description: dijkstra最短路径demo
+@Date: 2019-12-21 20:31:12
+@Author: Wong Symbol
+@LastEditors  : Wong Symbol
+@LastEditTime : 2019-12-21 21:09:11
+'''
 # -*- coding:utf-8 -*-
-
-'''
-简易dijkstra
-'''
 
 import numpy as np
 
@@ -15,12 +18,18 @@ mat = np.array([
     [0,0,0,0,3,0]
 ])
 
+# 记录已经访问过的节点下标
 aptSet = set()
+# 记录开始节点到其他节点的最短距离，列表下标与节点下标同
 dist = [float('inf')] * len(mat)
+print('start:')
+print(dist)
+print(aptSet)
+
 inf = float('inf')
+# 定义节点0为初始节点
 aptSet.add(0)
-# dist[0] = 0
-dist[0] = 0
+
 dist = [0, 3, 2, inf, inf, inf]                                                                                                 
 
 def updateDist(index):
@@ -32,11 +41,21 @@ def updateDist(index):
         if mat[index][i] > 0 and i not in aptSet:
             dist[i] = mat[index][i] + new
 
+while len(aptSet) < len(mat):
+    min_value = float('inf')
+    min_index = None
 
-# updateDist(0)
-# print(dist)
-# print(aptSet)
+    for i in dist:
+        if i < min_value and dist.index(i) not in aptSet:
+            min_index = dist.index(i)
+            min_value = i
+    updateDist(min_index)
+    
+print('end:')
+print(dist)
+print(aptSet)    
 
+'''
 # updateDist 的参数是下标
 updateDist(2)
 print(dist)
@@ -53,3 +72,4 @@ print(aptSet)
 updateDist(4)
 print(dist)
 print(aptSet)
+'''
