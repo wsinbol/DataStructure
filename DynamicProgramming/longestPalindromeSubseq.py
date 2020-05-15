8b205823a2ae34bb28b25760113e2c92b8a45216
wsinbol <wsinbol@gmail.com>
2020-05-15 14:15:51 +0800

    dp bag

--- DynamicProgramming/longestPalindromeSubseq.py
+++ DynamicProgramming/longestPalindromeSubseq.py
@@ -1,6 +1,16 @@
 
 '''
+最长回文子序列
 
+dp数组的运用：
+
+1) 涉及两个字符串/数组时（比如最长公共子序列），dp 数组的含义如下：
+
+在子数组 arr1[0..i] 和子数组 arr2[0..j] 中，我们要求的子序列（最长公共子序列）长度为 dp[i][j]
+
+2) 只涉及一个字符串/数组时（比如本文要讲的最长回文子序列），dp 数组的含义如下：
+
+在子数组 array[i..j] 中，我们要求的子序列（最长回文子序列）的长度为 dp[i][j]
 '''
 
 from collections import defaultdict
@@ -23,7 +33,8 @@ def longestPalindromeSubseq(s):
 			else:
 				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
 	'''
-
+	
+	
 	# 反着遍历
 	for i in range(num-1, 0-1, -1):
 		for j in range(i+1,num):
@@ -33,6 +44,8 @@ def longestPalindromeSubseq(s):
 				dp[i][j] = max(dp[i][j-1], dp[i+1][j])
 
 	print(dp)
+	# 返回 最长回文子串长度，即最右上角位置的值
+	return dp[0][num-1]
 
 s = 'abccba'
 longestPalindromeSubseq(s)
--- DynamicProgramming/simply_bag_value.py
+++ DynamicProgramming/simply_bag_value.py
@@ -5,6 +5,30 @@
 '''
 
 import numpy as np
+
+w = [2,1,3] # 每个物品的重量
+v = [4,2,3] # 每个物品的价值
+N = 3 # 物品个数
+W = 4 # 背包承受的最大重量
+
+# 对于 dp[i][j] 的理解一定要透彻
+# dp[i][j] 表示 对于前 i 个物品，当前背包的容量 j，这种情况装的最大价值是
+
+dp = np.zeros((N+1,W+1))
+
+for i in range(1,N+1):
+	for j in range(1,W+1):
+		# if j - w[i-1] < 0:
+		if j < w[i-1]:
+			dp[i][j] = dp[i-1][j]
+		else:
+			dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i-1]]+v[i-1])
+print(dp)
+exit()
+
+# 争哥版
+
+import numpy as np
 goods_num = 5 # 物品总个数
 goods_weight = [2,2,4,6,3] # 每个物品的重量
 goods_value = [3,4,8,9,6] # 对应物品的价值
